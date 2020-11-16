a5c9e8712edfbf429eeb07de980b1e298464dea9
Jacob Dale <jacobscottdale@gmail.com>
2020-11-16 10:10:13 -0500

    Adding show-router.js and show-service.js so client can alter the show table in database.

--- migrations/003.do.create_account_has_show.sql
+++ migrations/003.do.create_account_has_show.sql
@@ -1,6 +1,8 @@
 CREATE TABLE account_has_show (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
-  show_id INTEGER NOT NULL,
-  watch_status TEXT NOT NULL DEFAULT 'Unwatched'
+  trakt_id INTEGER NOT NULL,
+  watch_status TEXT NOT NULL,
+  foreign key (user_id) references accounts (user_id),
+  foreign key (trakt_id) references show (trakt_id)
 );
